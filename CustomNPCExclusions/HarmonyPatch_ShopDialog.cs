69fb06933968566456d9dc3908ccb6816a988741
Esca-MMC <esca.mmc@gmail.com>
2023-08-06 14:07:21 -0500

    Rewrite ShopDialog to simplify and fix freeze

--- CustomNPCExclusions/HarmonyPatch_ShopDialog.cs
+++ CustomNPCExclusions/HarmonyPatch_ShopDialog.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Threading;
 
 namespace CustomNPCExclusions
 {
@@ -17,9 +18,56 @@ namespace CustomNPCExclusions
             ModEntry.Instance.Monitor.Log($"Applying Harmony patch \"{nameof(HarmonyPatch_ShopDialog)}\": transpiling SDV method \"Game1.UpdateShopPlayerItemInventory(string, HashSet<NPC>)\".", LogLevel.Trace);
             harmony.Patch(
                 original: AccessTools.Method(typeof(Game1), nameof(Game1.UpdateShopPlayerItemInventory), new[] { typeof(string), typeof(HashSet<NPC>) }),
-                transpiler: new HarmonyMethod(typeof(HarmonyPatch_ShopDialog), nameof(Game1_UpdateShopPlayerItemInventory))
+                prefix: new HarmonyMethod(typeof(HarmonyPatch_ShopDialog), nameof(Game1_UpdateShopPlayerItemInventory))
             );
         }
+
+        /// <summary>Excludes a list of NPCs from randomly discussing items that players have sold to certain shops.</summary>
+        /// <param name="purchased_item_npcs">A list of NPCs to exclude from receiving shop dialog (normally used to avoid an NPC receiving multiple sets of dialog simultaneously).</param>
+        private static void Game1_UpdateShopPlayerItemInventory(ref HashSet<NPC> purchased_item_npcs)
+        {
+            try
+            {
+                List<string> excluded = new List<string>(); //a list of NPC names to exclude from giving or receiving gifts
+
+                foreach (KeyValuePair<string, List<string>> data in ModEntry.GetAllNPCExclusions()) //for each NPC's set of exclusion data
+                {
+                    if (data.Value.Exists(entry =>
+                        entry.StartsWith("All", StringComparison.OrdinalIgnoreCase) //if this NPC is excluded from everything
+                     || entry.StartsWith("TownEvent", StringComparison.OrdinalIgnoreCase) //OR if this NPC is excluded from town events
+                     || entry.StartsWith("ShopDialog", StringComparison.OrdinalIgnoreCase) //OR this NPC is excluded from dialog about the town shop
+                    ))
+                    {
+                        if (Game1.getCharacterFromName(data.Key) is NPC npc) //if the excluded NPC exists
+                        {
+                            excluded.Add(data.Key); //add their name to the exclusion list
+                            purchased_item_npcs.Add(npc); //add them to the set of NPCs who already received dialog this time
+                        }
+                    }
+                }
+
+                if (excluded.Count > 0 && ModEntry.Instance.Monitor.IsVerbose) //if any NPCs were excluded
+                {
+                    string logMessage = String.Join(", ", excluded);
+                    ModEntry.Instance.Monitor.Log($"Excluded NPCs from random shop dialog: {logMessage}", LogLevel.Trace);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModEntry.Instance.Monitor.LogOnce($"Harmony patch \"{nameof(Game1_UpdateShopPlayerItemInventory)}\" has encountered an error. The \"ShopDialog\" setting may not work correctly. Full error message: \n{ex.ToString()}", LogLevel.Error);
+                return; //run the original method
+            }
+        }
+
+
+
+
+
+
+
+
+        /*
+
 
         /// <summary>Replaces calls to <see cref="Utility.getRandomTownNPC()"/> with <see cref="GetRandomTownNPC_ShopDialogExclusions()"/>.</summary>
         public static IEnumerable<CodeInstruction> Game1_UpdateShopPlayerItemInventory(IEnumerable<CodeInstruction> instructions)
@@ -84,5 +132,7 @@ namespace CustomNPCExclusions
 
             return npc;
         }
+        
+        */
     }
 }
